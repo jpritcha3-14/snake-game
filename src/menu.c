380ae4d703db26b33421dac91be972facc7fde74
Jonathan Pritchard <jpritcha3.14@gmail.com>
2019-12-10 14:55:54 -0800

    add movable cursor to menu

--- src/menu.c
+++ src/menu.c
@@ -8,31 +8,69 @@
 
 enum option{start, speed, score, quit};
 
+struct cursor {
+    char* text;
+    char* blank;
+    int textlen;
+    int loc;
+};
+
+const char* text[] = {
+    "Start Game",
+    "Speed: Fast (to be implemented)",
+    "High Scores (to be implemented)",
+    "Quit"
+};
+
+void draw_cursor (wloc pa, struct cursor c) {
+     c.loc % 2 ? attron(COLOR_PAIR(2)) : attron(COLOR_PAIR(1));
+     mvaddstr(pa.y + c.loc * 2, pa.x - c.textlen - 1, c.text);
+     mvaddstr(pa.y + c.loc * 2 - 2, pa.x - c.textlen - 1, c.blank);
+     mvaddstr(pa.y + c.loc * 2 + 2, pa.x - c.textlen - 1, c.blank);
+     refresh();
+     attroff(COLOR_PAIR);
+}
+
+void draw_menu(wloc pa) {
+     for (int i = 0; i < 4; i++) {
+        i % 2 ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
+        mvaddstr(pa.y + 2 * i, pa.x, text[i]);
+     }
+     attroff(COLOR_PAIR);
+}
+
 int show_menu(wloc pa, WINDOW* dummy) {
     enum option selected = start;
-    char* start = "Start!";
-    char* quit = "Press q to quit";
-
     struct timespec w;
     w.tv_sec = 2;
     w.tv_nsec = 0;
-
+    struct cursor c = {"->", "  ", 2, 0};
     attron(A_BOLD);
-    mvaddstr(pa.y, pa.x, start);
-    mvaddstr(pa.y + 2, pa.x, quit);
-    refresh();
+    draw_menu(pa);
+    draw_cursor(pa, c);
+    char input;
     
-    while (wgetch(dummy) != 'q') {
-        play_game(pa, dummy);
-        nanosleep(&w, NULL);
-        clear();
-        mvaddstr(pa.y, pa.x, start);
-        mvaddstr(pa.y + 2, pa.x, quit);
-        refresh();
-        wtimeout(dummy, 0);
-        while (wgetch(dummy) != ERR);
-        wtimeout(dummy, -1);
+    for (;;) {
+        draw_menu(pa);
+        draw_cursor(pa, c);
+        input = wgetch(dummy); 
+        if (input == 'w' && c.loc > 0) {
+            c.loc--;  
+            draw_cursor(pa, c);
+        } else if (input == 's' && c.loc < 3) {
+            c.loc++; 
+            draw_cursor(pa, c);
+        } else if (input == ' ' && c.loc == 0) {
+            play_game(pa, dummy);
+            nanosleep(&w, NULL);
+            clear();
+            refresh();
+            wtimeout(dummy, 0);
+            while (wgetch(dummy) != ERR);
+            wtimeout(dummy, -1);
+        } else if (input == ' ' && c.loc == 3) {
+            break;
+        }
     }
-
     return 0;
 }
