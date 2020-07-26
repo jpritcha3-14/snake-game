183c70054b23b9293d5899883f59784d07053e24
Jonathan Pritchard <jpritcha3.14@gmail.com>
2020-07-25 17:03:54 -0700

    remove dummy WINDOW malloc, newwin returns allocated struct

--- src/main.c
+++ src/main.c
@@ -11,8 +11,7 @@ int main() {
     noecho();
     start_color();
     // Dummy window to for input, avoids extra refresh on wgetch call
-    WINDOW* dummy = malloc(sizeof(WINDOW));
-    dummy = newwin(1, 1, 1000, 1000); 
+    WINDOW* dummy = newwin(1, 1, 1000, 1000); 
     keypad(dummy, true);
     curs_set(0);
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
